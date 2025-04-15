1085113d26f44f40115f50c03a90eb541ff65a12
c0a2409471 <c0a2409471@edu.teu.ac.jp>
2025-04-15 15:51:41 +0900

    練習６：こうかとん横移動

--- pygame_image.py
+++ pygame_image.py
@@ -16,10 +16,10 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [tmr, 0])
         screen.blit(images,[300,200])
         pg.display.update()
-        tmr += 1        
+        tmr -= 1        
         clock.tick(200)
 
 
